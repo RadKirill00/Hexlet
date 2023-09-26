d5e499ba807e5f1f5002c6f23af64c8a96fe7139
Sergei Toptunov <toptunov.2005@mail.ru>
2023-09-26 21:13:35 +0300

    Комментарии

--- lesson1/les.js
+++ lesson1/les.js
@@ -1,2 +1,2 @@
-console.log('Hello, World!');
-console.log((8 / 4) - (8 + (2 - 1)) + 7 * 2);
+console.log('Hello, World!'); //Приветсвие
+console.log((8 / 4) - (8 + (2 - 1)) + 7 * 2);//Решение примера
