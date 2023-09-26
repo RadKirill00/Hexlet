659f0306de946c4c2daffb01542f474167183c1a
Sergei Toptunov <toptunov.2005@mail.ru>
2023-09-26 21:22:26 +0300

    Решение примеров

--- lesson1/les.js
+++ lesson1/les.js
@@ -1,3 +1,6 @@
 console.log('Hello, World!'); //Приветсвие
 console.log((8 / 4) - (8 + (2 - 1)) + 7 * 2);//Решение примера
 console.log(14 % 4);//?
+console.log(3 ** 5);//2
+console.log(-8 / -4);//3
+console.log(100 % 3);//4
