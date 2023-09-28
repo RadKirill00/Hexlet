070c73acf5371628bb824984f94e5942320c823c
Sergei Toptunov <toptunov.2005@mail.ru>
2023-09-28 16:31:11 +0300

    Исправление ошибки

--- lesson1/les.js
+++ lesson1/les.js
@@ -6,4 +6,4 @@ console.log(-8 / -4);//3
 console.log(100 % 3);//4
 console.log((3 ** 5) + (-8 / -4) + (100 % 3));
 console.log('Serega\nVarya\nNekit');
-console.log('- Did Joffrey agree?\n- He did. He also said \"I love using\\n\"');
+console.log('- Did Joffrey agree?\n- He did. He also said \"I love using \\n\"');
