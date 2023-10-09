5fbebe386a8a64bf1437135510c3ec3ef8135cb4
Sergei Toptunov <toptunov.2005@mail.ru>
2023-10-09 20:52:23 +0300

    Внёс изменение к задаче с return

--- practic/les2.js
+++ practic/les2.js
@@ -2,3 +2,7 @@ const pointInCircle = (a, b, r, x, y) => {
     const res = (x - a) ** 2 + (y - b) ** 2 <= r ** 2;
     return res;
   }
+console.log(pointInCircle(0, 0, 1, 0, 0) === true);
+ console.log(pointInCircle(0, 0, 1, 1, 0) === true);
+ console.log(pointInCircle(0, 0, 1, 1, 1) === false);
+  console.log(pointInCircle(0, 0, 1, Math.sqrt(0.8), 1 / 3) === true);
