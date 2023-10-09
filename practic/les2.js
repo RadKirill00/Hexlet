cc29edcf5f7983fa2984c37ef21fabc715bdedf6
Sergei Toptunov <toptunov.2005@mail.ru>
2023-10-09 21:08:45 +0300

    Решение задачи с логическим оператором И(&&)

--- practic/les2.js
+++ practic/les2.js
@@ -6,3 +6,11 @@ console.log(pointInCircle(0, 0, 1, 0, 0) === true);
  console.log(pointInCircle(0, 0, 1, 1, 0) === true);
  console.log(pointInCircle(0, 0, 1, 1, 1) === false);
   console.log(pointInCircle(0, 0, 1, Math.sqrt(0.8), 1 / 3) === true);
+  const isStrongPassword = (password) => {
+    const length = password.length;
+    return length > 8 && length < 24;
+  };
+  isStrongPassword('qwerty1');
+  isStrongPassword('qwerty1241531tv');
+  isStrongPassword('qwwkfdnbosrlkmbpfodmjofnemmno');
+  isStrongPassword('qwerty1103-M11');
